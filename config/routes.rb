607aec214a4b73f50eb89f42084bd468eaf28b08
v1rgul <v1rgul.42@gmail.com>
2015-11-28 23:59:46 +0100

    homepage with content

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root 'sellables#index'
+  root 'home#show'
   resources :sellable_types
   resources :sellables do
     get :autocomplete_sellable_type_name, :on => :collection
