4b5f34196682f7b7f56145665788b4048f95fa19
luigi21 <luigi21.thomas@gmail.com>
2015-11-29 11:39:51 +0100

    Last

--- HTML_DEV/js/main.js
+++ HTML_DEV/js/main.js
@@ -50,3 +50,30 @@ jQuery(document).ready(function($){
 		}
 	}
 });
+
+
+
+
+
+//Filter News
+$('select#sort-news').change(function() {
+	var filter = $(this).val()
+	filterList(filter);
+});
+
+//News filter function
+function filterList(value) {
+	var list = $(".news-list .news-item");
+	$(list).fadeOut("fast");
+	if (value == "tout") {
+		$(".news-list").find("article").each(function (i) {
+			$(this).delay(200).slideDown("fast");
+		});
+	} else {
+		//Notice this *=" <- This means that if the data-category contains multiple options, it will find them
+		//Ex: data-category="Cat1, Cat2"
+		$(".news-list").find("article[data-category*=" + value + "]").each(function (i) {
+			$(this).delay(200).slideDown("fast");
+		});
+	}
+}
