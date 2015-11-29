d556075f40327e4588ec4e7dee4953da930480ca
v1rgul <v1rgul.42@gmail.com>
2015-11-29 11:06:15 +0100

    auto generating tabs

--- app/controllers/member/base_controller.rb
+++ app/controllers/member/base_controller.rb
@@ -1,6 +1,14 @@
 class Member::BaseController < ApplicationController
   before_action :authenticate_user!
   before_action :bouhou, except: [:show, :index]
+  before_action :tabs
+
+  def tabs
+	@tabs = [{ name: "Factures", content: %{
+		}},{ name: "Facture", content: %{
+		}},{ name: "Membres", content: %{
+		}}];
+  end
 
   def bouhou
     unless current_user.is_member?
