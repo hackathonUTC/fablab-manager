4fb023a28aaf5581f89277fb72ac19ddcaf1cad0
luigi21 <luigi21.thomas@gmail.com>
2015-11-29 05:40:30 +0100

    nouveau menu, nouvelle présentation (onglets)

--- HTML_DEV/js/main.js
+++ HTML_DEV/js/main.js
@@ -1,125 +1,52 @@
-// Effet flou
-
-jQuery(document).ready(function ($) {
-    
-    //clipped image - blur effect
-	set_clip_property();
-    
-	$(window).on('resize', function () {
-		set_clip_property();
-	});
-
-	function set_clip_property() {
-		var $header_height = $('header').height(),
-			$window_width = $(window).width();
-		$('.blurred-bg').css('clip', 'rect(0px, ' + $window_width + 'px, ' + $header_height + 'px, 0px)');
-	}
-    
-});
-
-
-
-
-// Title-Opacity changed when scrool
-
-
-var header = $('#page-title');
-var range = 225;
-
-$(window).on('scroll', function () {
-
-    var scrollTop = $(this).scrollTop(),
-		offset = header.offset().top,
-		height = header.outerHeight(),
-		offset = offset + height / 2,
-		calc = 1 - (scrollTop - offset + range) / range;
-
-    header.css({
-        'opacity': calc
-    });
-
-
-    if (calc > '1') {
-        header.css({
-            'opacity': 1
-        });
-    } else if (calc < '0') {
-        header.css({
-            'opacity': 0
-        });
-    }
-    
-});
-
-
-
-
-
-
-// CONTACT
-
-jQuery(document).ready(function(){
-	var modalTriggerBts = $('a[data-type="cd-modal-trigger"]'),
-		coverLayer = $('.cd-cover-layer');
+jQuery(document).ready(function($){
+	var tabs = $('.cd-tabs');
 	
-	/*
-		convert a cubic bezier value to a custom mina easing
-		http://stackoverflow.com/questions/25265197/how-to-convert-a-cubic-bezier-value-to-a-custom-mina-easing-snap-svg
-	*/
-	var duration = 600,
-		epsilon = (1000 / 60 / duration) / 4,
-		firstCustomMinaAnimation = bezier(.63,.35,.48,.92, epsilon);
+	tabs.each(function(){
+		var tab = $(this),
+			tabItems = tab.find('ul.cd-tabs-navigation'),
+			tabContentWrapper = tab.children('ul.cd-tabs-content'),
+			tabNavigation = tab.find('nav');
 
-	modalTriggerBts.each(function(){
-		initModal($(this));
+		tabItems.on('click', 'a', function(event){
+			event.preventDefault();
+			var selectedItem = $(this);
+			if( !selectedItem.hasClass('selected') ) {
+				var selectedTab = selectedItem.data('content'),
+					selectedContent = tabContentWrapper.find('li[data-content="'+selectedTab+'"]'),
+					slectedContentHeight = selectedContent.innerHeight();
+				
+				tabItems.find('a.selected').removeClass('selected');
+				selectedItem.addClass('selected');
+				selectedContent.addClass('selected').siblings('li').removeClass('selected');
+				//animate tabContentWrapper height when content changes 
+				tabContentWrapper.animate({
+					'height': slectedContentHeight
+				}, 200);
+			}
+		});
+
+		//hide the .cd-tabs::after element when tabbed navigation has scrolled to the end (mobile version)
+		checkScrolling(tabNavigation);
+		tabNavigation.on('scroll', function(){ 
+			checkScrolling($(this));
+		});
+	});
+	
+	$(window).on('resize', function(){
+		tabs.each(function(){
+			var tab = $(this);
+			checkScrolling(tab.find('nav'));
+			tab.find('.cd-tabs-content').css('height', 'auto');
+		});
 	});
 
-	function initModal(modalTrigger) {
-		var modalTriggerId =  modalTrigger.attr('id'),
-			modal = $('.cd-modal[data-modal="'+ modalTriggerId +'"]'),
-			svgCoverLayer = modal.children('.cd-svg-bg'),
-			paths = svgCoverLayer.find('path'),
-			pathsArray = [];
-		//store Snap objects
-		pathsArray[0] = Snap('#'+paths.eq(0).attr('id')),
-		pathsArray[1] = Snap('#'+paths.eq(1).attr('id')),
-		pathsArray[2] = Snap('#'+paths.eq(2).attr('id'));
-
-		//store path 'd' attribute values	
-		var pathSteps = [];
-		pathSteps[0] = svgCoverLayer.data('step1');
-		pathSteps[1] = svgCoverLayer.data('step2');
-		pathSteps[2] = svgCoverLayer.data('step3');
-		pathSteps[3] = svgCoverLayer.data('step4');
-		pathSteps[4] = svgCoverLayer.data('step5');
-		pathSteps[5] = svgCoverLayer.data('step6');
-		
-		//open modal window
-		modalTrigger.on('click', function(event){
-			event.preventDefault();
-			modal.addClass('modal-is-visible');
-			coverLayer.addClass('modal-is-visible');
-			animateModal(pathsArray, pathSteps, duration, 'open');
-		});
-
-		//close modal window
-		modal.on('click', '.modal-close', function(event){
-			event.preventDefault();
-			modal.removeClass('modal-is-visible');
-			coverLayer.removeClass('modal-is-visible');
-			animateModal(pathsArray, pathSteps, duration, 'close');
-		});
-	}
-
-	function animateModal(paths, pathSteps, duration, animationType) {
-		var path1 = ( animationType == 'open' ) ? pathSteps[1] : pathSteps[0],
-			path2 = ( animationType == 'open' ) ? pathSteps[3] : pathSteps[2],
-			path3 = ( animationType == 'open' ) ? pathSteps[5] : pathSteps[4];
-		paths[0].animate({'d': path1}, duration, firstCustomMinaAnimation);
-		paths[1].animate({'d': path2}, duration, firstCustomMinaAnimation);
-		paths[2].animate({'d': path3}, duration, firstCustomMinaAnimation);
+	function checkScrolling(tabs){
+		var totalTabWidth = parseInt(tabs.children('.cd-tabs-navigation').width()),
+		 	tabsViewport = parseInt(tabs.width());
+		if( tabs.scrollLeft() >= totalTabWidth - tabsViewport) {
+			tabs.parent('.cd-tabs').addClass('is-ended');
+		} else {
+			tabs.parent('.cd-tabs').removeClass('is-ended');
+		}
 	}
 });
-
-
-
