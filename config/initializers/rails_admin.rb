74d064f246ce5318c67c5a77c4b0bf3520a18964
DSI <dsi@usec-utc.fr>
2015-11-29 07:42:49 +0100

    Correction probleme authentification

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -8,7 +8,7 @@ RailsAdmin.config do |config|
   # end
   # config.current_user_method(&:current_user)
   config.authorize_with do
-    redirect_to main_app.root_path unless current_user.is_admin
+    redirect_to main_app.root_path unless !current_user.nil? && current_user.is_admin?
   end
 
   ## == Cancan ==
