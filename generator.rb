678c15a550341ad01b8c42379cb62a7af6b26610
DSI <dsi@usec-utc.fr>
2016-02-07 15:03:03 +0100

    Clean old files

--- generator.rb
+++
@@ -1,4 +0,0 @@
-rails g scaffold Invoice
-rails g model Order quantite:integer reduction:float invoice:references
-rails g scaffold Sellable unit:string name:string description:string stock:integer price_per_unit_CI:float price_per_unit_permanencier:float price_per_unit_nc:float price_per_unit_c:float sellable_type:references
-rails g scaffold SellableType name:string
